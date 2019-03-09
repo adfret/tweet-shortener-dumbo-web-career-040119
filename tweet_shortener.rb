fa2b00e6e11bd0ae3e7a3fb9c7725736d55c4e8d
Ad Friedman <adien330@yahoo.com>
2019-03-09 22:06:10 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def dictionary
+  shortener = {"hello":"hi", "two": 2, "to": 2, "too": 2, "for": 4, "four": 4, "be": "b", "you": "u",
+  "at": "@", "and": "&"}
+end
+
+
+def word_substituter(string)
+end
+  # Write your code here.
