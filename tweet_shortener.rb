d6a842ab6c2612c61d338b83a3579b5d29aa1433
Ad Friedman <adien330@yahoo.com>
2019-03-12 19:02:32 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -3,7 +3,58 @@ def dictionary
   "at": "@", "and": "&"}
 end
 
-
 def word_substituter(string)
+    arr,arrs = [], []
+    string.split.each do |i|
+      arr << i.to_sym
+      end
+
+    arr.each do |i|
+        if !dictionary.include?i.downcase
+        arrs << i.to_s
+        else
+        arrs << dictionary[i.downcase]
+        end
+    end
+
+    arrs.join(" ")
+
 end
-  # Write your code here.
+
+
+def bulk_tweet_shortener(tweets)
+  arr = []
+  tweets.each do |i|
+    arr << word_substituter(i)
+  end
+
+arr.each do |i|
+  puts i
+end
+
+end
+
+def selective_tweet_shortener(tweet)
+arrs = []
+      if tweet.length > 140
+        arrs << word_substituter(tweet)[0..140]
+      elsif word_substituter(tweet).length < 130
+        arrs << tweet
+      else
+        arrs << tweet
+      end
+
+arrs.join(" ")
+end
+
+
+def shortened_tweet_truncator(tweet)
+tweet = word_substituter(tweet)
+
+if tweet.length > 140
+  tweet[0..136] + "..."
+  else
+  tweet
+  end
+
+end
